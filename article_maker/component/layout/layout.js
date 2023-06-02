6bd7ccd71c1ae3033a53fb4b68337fd79db48efb
yunhwan <yhyoon1004@naver.com>
2023-06-03 00:48:13 +0900

    react 헤더 초기 작업

---
+++ article_maker/component/layout/layout.js
@@ -0,0 +1,12 @@
+import Header from "@/component/layout/header/header";
+
+export default function Layout(props) {
+
+
+    return (
+        <>
+            <Header/>
+
+        </>
+    )
+}
--- article_maker/pages/index.js
+++ article_maker/pages/index.js
@@ -1,28 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import {Col, Container, Row} from "react-bootstrap";
-import {useRouter} from "next/router";
+import Layout from "@/component/layout/layout";
 
 
 export default function Home() {
-    const router = useRouter();
-
-  const border ={
-    border: "solid 1px grey"
-  }
-  const onClickHandler =(url)=>{
-      router.push(url);
-  }
-  return (
-      <Container className="container-fluid" style={border}>
-        <Row>
-          <Col className="col-md-3" style={border} onClick={onClickHandler("/")}>메인페이지</Col>
-          <Col className="col-md-3" style={border}>기사 작성하기</Col>
-          <Col className="col-md-3" style={border}>기사 목록보기</Col>
-          <Col className="col-md-3" style={border}>관리자</Col>
-        </Row>
-      </Container>
-  )
+    return(
+        <>
+            <Layout />
+        </>
+    )
 }
