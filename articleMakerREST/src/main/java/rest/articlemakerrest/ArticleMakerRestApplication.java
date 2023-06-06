32179eb1085d2542c49f2583430d61650d5d5437
yunhwan <yhyoon1004@naver.com>
2023-06-06 14:59:26 +0900

    REST 재설치

---
+++ articleMakerREST/src/main/java/rest/articlemakerrest/ArticleMakerRestApplication.java
@@ -0,0 +1,13 @@
+package rest.articlemakerrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleMakerRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArticleMakerRestApplication.class, args);
+    }
+
+}
---
+++ articleMakerREST/src/test/java/rest/articlemakerrest/ArticleMakerRestApplicationTests.java
@@ -0,0 +1,13 @@
+package rest.articlemakerrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticleMakerRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
