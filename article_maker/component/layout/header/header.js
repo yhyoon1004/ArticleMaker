d7ef687a241ec116db87df5f0b40b4c494f2f9fb
yunhwan <yhyoon1004@naver.com>
2023-06-07 20:58:42 +0900

    기사 작성 창 팝업 설정

--- article_maker/component/layout/header/header.js
+++ article_maker/component/layout/header/header.js
@@ -2,6 +2,12 @@ import {Col, Container, Row} from "react-bootstrap";
 import Link from "next/link";
 
 export default function Header() {
+    // function
+    const popUp = (url)=>{
+        window.open(url,"기사 편집기","popup=yes");
+    }
+
+    // CSS
     const headerCSS = {
         border: "solid 1px grey",
         backgroundColor: "lightblue",
@@ -15,9 +21,10 @@ export default function Header() {
                 <Col className="col-md-3" style={headerCSS}>
                     <Link href="/" style={cssNone}>메인페이지</Link>
                 </Col>
-                <Col className="col-md-3" style={headerCSS}>
-                    <Link href="/writeArticlePage" style={cssNone}>기사
-                        작성하기</Link>
+                <Col className="col-md-3" style={headerCSS}
+                onClick={()=>{popUp("/writeArticlePage")}}>
+                        기사
+                        작성하기
                 </Col>
                 <Col className="col-md-3" style={headerCSS}>
                     <Link href="/articleListPage/" style={cssNone}>기사 목록보기</Link>
