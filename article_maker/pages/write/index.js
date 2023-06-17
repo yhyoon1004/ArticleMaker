42d9f8bdc1fbd1e96bbeadca06b95090eea9c650
yunhwan <yhyoon1004@naver.com>
2023-06-17 20:56:55 +0900

    FE - write페이지 REST서버-DB article기사 등록 구현

--- article_maker/pages/write/index.js
+++ article_maker/pages/write/index.js
@@ -3,24 +3,51 @@ import Editor from "@/component/editor/Editor";
 import {useEffect, useState} from "react";
 import {Button, Col, Container, Row} from "react-bootstrap";
 import SideBar from "@/component/editor/SideBar";
+import axios from "axios";
 
 export default function Index() {
     const [editorLoaded, setEditorLoaded] = useState(false);
-    const [data, setData] = useState("default");
+    const [articleContent, setArticleContent] = useState("default");
 
     useEffect(() => {
         setEditorLoaded(true);
     }, []);
 
-    return(
+    const onClickSave = async () => {
+        try {
+            await axios({
+                url:"http://localhost:8080/articleInsert",
+                method: "post",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                data: {
+                    "id": null,
+                    "status": 0,
+                    "writer_id": "yh",
+                    "title": "titletemp",
+                    "content": articleContent,
+                    "create_time": null,
+                    "published_time": null,
+                    "view_count": 0
+                }
+
+                }
+            ).catch();
+        } catch (e){
+
+        }
+    }
+
+    return (
         <Layout>
             <Container fluid>
-                <Row style={{minHeight:500}}>
-                    <Col md={8} style={{minHeight:500}}>
+                <Row style={{minHeight: 500}}>
+                    <Col md={8} style={{minHeight: 500}}>
                         <Editor
                             name="description"
                             onChange={(data) => {
-                                setData(data);
+                                setArticleContent(data);
                             }}
                             editorLoaded={editorLoaded}
                         />
@@ -29,15 +56,15 @@ export default function Index() {
                         <SideBar/>
                     </Col>
                 </Row>
-                <Row >
+                <Row>
                     <Col md={8}>
 
                     </Col>
 
                     <Col md={4}>
-                        <Button className="p-1 ms-1" variant="secondary" >취소</Button>
-                        <Button className="p-1 ms-1"  variant="secondary">임시 저장</Button>
-                        <Button className="p-1 ms-1"  variant="secondary">송고</Button>
+                        <Button className="p-1 ms-1" variant="secondary">취소</Button>
+                        <Button className="p-1 ms-1" variant="secondary">임시 저장</Button>
+                        <Button className="p-1 ms-1" variant="secondary" onClick={onClickSave}>송고</Button>
                     </Col>
                 </Row>
             </Container>
