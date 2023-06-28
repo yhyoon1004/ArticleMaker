24c6acabbc27e94c21820a83bc90c7eaf648fdc0
yunhwan <yhyoon1004@naver.com>
2023-06-28 16:12:27 +0900

    FE - 동영상 업로드 구현 3

--- article_maker/pages/video/videoUploader.js
+++ article_maker/pages/video/videoUploader.js
@@ -1,31 +1,49 @@
 import {FileUploader} from "react-drag-drop-files";
 import {useState} from "react";
 import {Container, Row, Form, Col, Card} from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import axios from "axios";
 
-const fileTypes = ["JPG", "PNG", "GIF"];
 export default function VideoUploader() {
     const [file, setFile] = useState(null);
     const handleChange = (file) => {
         setFile(file);
     };
+
+    const submitHandling = () => {
+        axios({
+            url: "http://localhost:8080/videoUpload",
+            method: "POST",
+            headers: {
+                "Content-Type": "multipart/form-data"
+            },
+            data: file
+        }).then((response)=>{
+            if(response){
+
+            }
+
+
+        })
+    };
+
     return (
-        <Card className={"m-3"} >
-            <Container fluid >
-                <Row>
-                    <Col md={12}>
-                        <Form.Group controlId="formFileSm" className="mb-3">
-                            <Form.Label>동영상 업로드</Form.Label>
-                            <Form.Control type="file" size="sm"/>
-                            <FileUploader
-                                handleChange={handleChange}
-                                name="file"
-                                types={fileTypes}/>
-                        </Form.Group>
-                    </Col>
-
-                </Row>
-
-            </Container>
+        <Card className={"m-3"} style={{minWidth:"322px",maxWidth:"508px",minHeight:"200px"}}>
+            <Card.Header as="h5">파일 업로더</Card.Header>
+            <Card.Body>
+                <FileUploader
+                    handleChange={handleChange}
+                    name="videoUploadForm"
+                    types={["JPG", "PNG", "GIF", "TXT"]}
+                    onDrop={() => console.log(file)}
+                    fileOrFiles={["a","b"]}
+                    multiple={true}
+                />
+            </Card.Body>
+            <div style={{display: "flex", justifyContent: "center"}}>
+                <Button variant="primary" className="m-2" onClick={submitHandling}> 업로드 </Button>
+                <Button variant="secondary" className="m-2" onClick={() => window.close()}> 취소 < /Button>
+            </div>
         </Card>
     )
 };
