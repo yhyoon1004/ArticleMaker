4a0328bce30734f290768da2ab0796db17aeb5a6
yunhwan <yhyoon1004@naver.com>
2023-08-26 18:48:05 +0900

    cafe24 가상호스트 pc 배포 에러 catch()처리2

--- article_maker/hook/useGetArticleList.js
+++ article_maker/hook/useGetArticleList.js
@@ -1,9 +1,14 @@
 import axios from "axios";
 
 export async function useGetArticleList(articleURL, parameter) {
-    return await axios().get({
-            url: articleURL,
-            params: Object.fromEntries(parameter)
-        }
-    ).catch();
+    try {
+        return await axios().get({
+                url: articleURL,
+                params: Object.fromEntries(parameter)
+            }
+        ).catch();
+    } catch (e){
+        return null;
+    }
+
 }
--- article_maker/queryFunction/getArticleList.js
+++ article_maker/queryFunction/getArticleList.js
@@ -1,12 +1,17 @@
 import axios from "axios";
 
 export async function getArticleList(targetUrl, targetParameter) {
-    return await axios({
-        url: targetUrl,
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        params: targetParameter,
-    }).catch();
+    try{
+        return await axios({
+            url: targetUrl,
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            params: targetParameter,
+        });
+    }catch (e){
+        return null;
+    }
+
 }
